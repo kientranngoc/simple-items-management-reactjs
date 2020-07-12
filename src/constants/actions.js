3df655665a60b2fe917fb2e0e6bc4bb7a19754f7
Tran Kien <kien@gotitapp.co>
2020-07-12 15:47:21 +0700

    feat: Add action constants

---
+++ src/constants/actions.js
@@ -0,0 +1,7 @@
+export const SIGNUP = "SIGNUP";
+export const SIGNIN = "SIGNIN";
+export const FETCH_CATEGORIES = "FETCH_CATEGORIES";
+export const FETCH_CATEGORY = "FETCH_CATEGORY";
+export const FETCH_ITEMS = "FETCH_ITEMS";
+export const ADD_ITEM = "ADD_ITEM";
+export const UPDATE_ITEM = "UPDATE_ITEM";
