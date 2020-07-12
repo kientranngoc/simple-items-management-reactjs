95a2f1886d7ae92a9d0e939614879932d0a96288
Tran Kien <kien@gotitapp.co>
2020-07-12 11:31:48 +0700

    feat: Correct the route order, add Switch to the routes

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { PropTypes } from "prop-types";
 import Home from "./Home";
-import Signup from "./Signin";
+import Signup from "./Signup";
 import Signin from "./Signin";
 import CategoryDetail from "./CategoryDetail";
 
 const Root = ({ store }) => (
   <Provider store={store}>
     <Router>
-      <Route path="/" component={Home} />
-      <Route path="/signup" component={Signup} />
-      <Route path="/signin" component={Signin} />
-      <Route path="/category/:id" component={CategoryDetail} />
+      <Switch>
+        <Route path="/signup" component={Signup} />
+        <Route path="/signin" component={Signin} />
+        <Route path="/category/:id" component={CategoryDetail} />
+        <Route path="/" component={Home} />
+      </Switch>
     </Router>
   </Provider>
 );
